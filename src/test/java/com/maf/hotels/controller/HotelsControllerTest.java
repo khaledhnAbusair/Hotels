1e9d9a6e23a0bf96c6bca4e564c5540f711a53e6
khaledabusair <khaled.hnabusair@gmail.com>
2022-06-05 03:03:41 +0300

    fix integration test

--- src/test/java/com/maf/hotels/controller/HotelsControllerTest.java
+++ src/test/java/com/maf/hotels/controller/HotelsControllerTest.java
@@ -1,10 +1,9 @@
 package com.maf.hotels.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.maf.hotels.client.BestHotelClient;
 import com.maf.hotels.client.CrazyHotelClient;
-import com.maf.hotels.mapper.BestMapper;
-import com.maf.hotels.mapper.CrazyMapper;
 import com.maf.hotels.model.BestHotelResponse;
 import com.maf.hotels.model.CrazyHotelResponse;
 import com.maf.hotels.model.HotelResponse;
@@ -12,6 +11,7 @@ import feign.FeignException;
 import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -25,12 +25,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -41,47 +40,53 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 class HotelsControllerTest {
-    @Autowired
-    MockMvc mockMvc;
-
-    @MockBean
-    BestHotelClient bestHotelClient;
     @MockBean
     CrazyHotelClient crazyHotelClient;
 
+    @MockBean
+    BestHotelClient bestHotelClient;
+    @Autowired
+    MockMvc mockMvc;
+
+    private final LocalDate fromDate = LocalDate.parse("2018-03-29");
+    private final LocalDate toDate = LocalDate.parse("2018-04-05");
+    private List<CrazyHotelResponse> crazyHotelResponses;
+    private List<BestHotelResponse> bestHotelResponses;
+    private final Gson gson = new Gson();
+    private final String city = "TST";
+
+    @BeforeEach
+    @SneakyThrows
+    void setUp() {
+        crazyHotelResponses = List.of(loadResponse(CrazyHotelResponse.class, "crazy-hotel.json"));
+        bestHotelResponses = List.of(loadResponse(BestHotelResponse.class, "best-hotel.json"));
+    }
 
     @Test
     @SneakyThrows
     void whenCallAvailableHotelWithValidParamsThenReturnListOfHotelResponse() {
-        BestHotelResponse bestHotelResponses = loadResponse(BestHotelResponse.class, "best-hotel.json");
-        Mockito.when(bestHotelClient.get(LocalDate.now(), LocalDate.now(), "AAA", 1)).thenReturn(List.of(bestHotelResponses));
+        Mockito.when(crazyHotelClient.get(toInstant(fromDate), toInstant(toDate), city, 1)).thenReturn(crazyHotelResponses);
+        Mockito.when(bestHotelClient.get(fromDate, toDate, city, 1)).thenReturn(bestHotelResponses);
 
-
-        CrazyHotelResponse crazyHotelResponse = loadResponse(CrazyHotelResponse.class, "crazy-hotel.json");
-        Mockito.when(crazyHotelClient.get(LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC),
-                LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC), "AAA", 1)).thenReturn(List.of(crazyHotelResponse));
-
-        List<HotelResponse> hotelResponses = Stream.of(BestMapper.INSTANCE.map(bestHotelResponses), CrazyMapper.INSTANCE.map(crazyHotelResponse))
-                .collect(Collectors.toList());
 
         this.mockMvc.perform(get("/AvailableHotel")
                         .param("fromDate", "2018-03-29")
                         .param("toDate", "2018-04-05")
-                        .param("city", "AAA")
-                        .param("numberOfAdults", "1")
-                )
+                        .param("city", city)
+                        .param("numberOfAdults", "1"))
 
                 .andExpect(status().isOk())
                 .andExpect(result -> {
-                    Assertions.assertNotNull(result.getResponse().getContentAsString());
+                    String content = result.getResponse().getContentAsString();
+                    Assertions.assertNotNull(content);
+                    List<HotelResponse> hotelResponses = gson.fromJson(content, new TypeToken<List<HotelResponse>>() {
+                    }.getType());
                     Assertions.assertNotNull(hotelResponses);
                     Assertions.assertEquals(2, hotelResponses.size());
                     Assertions.assertTrue(hotelResponses.stream().anyMatch(hotelResponse -> hotelResponse.getProvider().equals("BestHotels")));
                     Assertions.assertTrue(hotelResponses.stream().anyMatch(hotelResponse -> hotelResponse.getProvider().equals("CrazyHotels")));
                     Assertions.assertTrue(hotelResponses.stream().anyMatch(hotelResponse -> hotelResponse.getHotelName().equals("A Best hotel")));
                     Assertions.assertTrue(hotelResponses.stream().anyMatch(hotelResponse -> hotelResponse.getHotelName().equals("A crazy hotel")));
-                    Mockito.verify(bestHotelClient, Mockito.times(1))
-                            .get(LocalDate.parse("2018-03-29"), LocalDate.parse("2018-04-05"), "AAA", 1);
                 }).andDo(print());
     }
 
@@ -89,26 +94,22 @@ class HotelsControllerTest {
     @Test
     @SneakyThrows
     void whenCallAvailableHotelWithValidParamsAndSomeProviderErrorThenReturnListOfOtherProviderHotelResponse() {
-        BestHotelResponse bestHotelResponses = loadResponse(BestHotelResponse.class, "best-hotel.json");
-        Mockito.when(bestHotelClient.get(LocalDate.now(), LocalDate.now(), "AAA", 1)).thenReturn(List.of(bestHotelResponses));
+        Mockito.when(crazyHotelClient.get(toInstant(fromDate), toInstant(toDate), city, 1)).thenThrow(FeignException.FeignClientException.class);
+        Mockito.when(bestHotelClient.get(fromDate, toDate, city, 1)).thenReturn(bestHotelResponses);
 
-        Mockito.when(crazyHotelClient.get(LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC),
-                LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC), "AAA", 1)).thenThrow(FeignException.FeignClientException.class);
-
-        List<HotelResponse> hotelResponses = Stream.of(BestMapper.INSTANCE.map(bestHotelResponses))
-                .collect(Collectors.toList());
 
         this.mockMvc.perform(get("/AvailableHotel")
                         .param("fromDate", "2018-03-29")
                         .param("toDate", "2018-04-05")
-                        .param("city", "AAA")
-                        .param("numberOfAdults", "1")
-                )
+                        .param("city", city)
+                        .param("numberOfAdults", "1"))
 
                 .andExpect(status().isOk())
                 .andExpect(result -> {
-                    Assertions.assertNotNull(result.getResponse().getContentAsString());
-                    Assertions.assertNotNull(hotelResponses);
+                    String content = result.getResponse().getContentAsString();
+                    Assertions.assertNotNull(content);
+                    List<HotelResponse> hotelResponses = new Gson().fromJson(content, new TypeToken<List<HotelResponse>>() {
+                    }.getType());
                     Assertions.assertEquals(1, hotelResponses.size());
                     Assertions.assertTrue(hotelResponses.stream().anyMatch(hotelResponse -> hotelResponse.getProvider().equals("BestHotels")));
                     Assertions.assertFalse(hotelResponses.stream().anyMatch(hotelResponse -> hotelResponse.getProvider().equals("CrazyHotels")));
@@ -117,22 +118,26 @@ class HotelsControllerTest {
                 }).andDo(print());
     }
 
+
     @Test
     @SneakyThrows
     void whenCallAvailableHotelWithMissingRequiredParamsThenHttpStatusIsBadRequest() {
         this.mockMvc.perform(get("/AvailableHotel")
                         .param("fromDate", "2018-03-29")
-                        .param("city", "AAA")
+                        .param("city", city)
                         .param("numberOfAdults", "1"))
                 .andExpect(status().isBadRequest()).andDo(print());
     }
 
     private <T> T loadResponse(Class<T> tClass, String fileName) throws IOException {
-        Gson gson = new Gson();
         ClassLoader classLoader = gson.getClass().getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
         String jsonString = IOUtils.toString(Objects.requireNonNull(inputStream), String.valueOf(StandardCharsets.UTF_8));
         return gson.fromJson(jsonString, tClass);
+    }
+
+    private Instant toInstant(LocalDate localDate) {
+        return localDate.atStartOfDay().toInstant(ZoneOffset.UTC);
     }
 
 }
